0e6423c84f19c550a6ef56741254740cf4b59aa2
Bryan Thompson <bryan.thompson@unisys.com>
2016-05-19 14:09:46 -0400

    staging: unisys: visorbus: Make visordriver_callback_lock a mutex
    visordriver_callback_lock is just a binary semaphore that logically
    makes more sense as a mutex.
    
    Signed-off-by: Bryan Thompson <bryan.thompson@unisys.com>
    Signed-off-by: David Kershner <david.kershner@unisys.com>
    Reviewed-by: Tim Sell <Timothy.Sell@unisys.com>

--- drivers/staging/unisys/include/visorbus.h
+++ drivers/staging/unisys/include/visorbus.h
@@ -161,7 +161,8 @@ struct visor_device {
 	struct timer_list timer;
 	bool timer_active;
 	bool being_removed;
-	struct semaphore visordriver_callback_lock;
+	/* mutex to serialize visor_driver function callbacks */
+	struct mutex visordriver_callback_lock;
 	bool pausing;
 	bool resuming;
 	u32 chipset_bus_no;
